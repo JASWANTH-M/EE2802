ae440d8707b2120706e868caf35749ae2868d0b7
JASWANTH-M <97540840+JASWANTH-M@users.noreply.github.com>
2023-01-21 13:36:37 +0530

    Add files via upload

---
+++ 11.10.1.2/codes/code.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# Let the length of the side of equilateral triangle be 5
+a = 5
+
+y1 = 
+# Given one side of triangel lies on y-axis
+B = np.array([0,y1])
+C = np.array([0,y2])
+
+
