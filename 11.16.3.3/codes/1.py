4e466f4fb652857d41bbfb45c5266ff511054e61
Jaswanth-M <madalajaswanth12@gmail.com>
2023-04-02 08:56:17 +0530

    CDF fn, plot

--- 11.16.3.3/codes/1.py
+++ 11.16.3.3/codes/1.py
@@ -2,18 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Define the sample space of the die roll
-sample_space = [0,1, 2, 3, 4, 5, 6,7]
+sample_space = [0, 1, 2, 3, 4, 5, 6, 7]
 
 # Define the probability distribution of the die roll
-prob_dist = [0,0,1/6, 1/6, 1/6, 1/6, 1/6, 1/6]
+prob_dist = [0, 1/6, 1/6, 1/6, 1/6, 1/6, 1/6, 0]
 
 # Calculate the cumulative probabilities for the die roll
 cumulative_prob = np.cumsum(prob_dist)
 
-# Plot the CDF
-plt.step(sample_space, cumulative_prob)
-plt.title("Cumulative Distribution Function")
+# Plot the CDF using a stem plot
+plt.stem(cumulative_prob)
+plt.title("Cumulative Distribution Function for a Die Roll")
 plt.xlabel("Number on the Die")
-plt.grid('on')
 plt.ylabel("Cumulative Probability")
 plt.savefig("../figs/fig.png")
